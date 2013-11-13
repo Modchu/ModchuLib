0bbf53ff6e6d36df9c0338d48f41cc53a9fa0fda
modchu <kawd82jstskl@yahoo.co.jp>
2013-11-13 11:35:20 +0900

    1.6.4 v7

--- net/minecraft/src/Modchu_ItemRenderer.java
+++
@@ -1,9 +0,0 @@
-package net.minecraft.src;
-
-public abstract class Modchu_ItemRenderer extends MMM_ItemRenderer {
-
-	public Modchu_ItemRenderer(Minecraft minecraft) {
-		super(minecraft);
-	}
-
-}
